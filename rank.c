f6f73fa593f16b46f91af4db3963155cb44f7f47
kusainovaka <kusainovaka@gmail.com>
2017-09-25 22:52:05 +0600

    44

--- rank.c
+++ rank.c
@@ -1,20 +1,36 @@
 //Fullname: Kamila Kusainova
 //Group: 3EN04A
-#include <stdlib.h>
+
+#include <stdio.h>
+#include <string.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-#define BUFFER_SIZE 50
-
-int main(int argc, char **argv)
+ int main(int argc, char **argv)
 {
+  int array[5];
+    int i, j, num, temp;
  int c;
 FILE *file;
 file = fopen("text.txt", "r");
 if (file) {
     while ((c = getc(file)) != EOF)
+
+    //   for (i = 0; i < num; i++)
+    // {
+    //     for (j = 0; j < (num - i - 1); j++)
+    //     {
+    //         if (array[j] > array[j + 1])
+    //         {
+    //             temp = array[j];
+    //             array[j] = array[j + 1];
+    //             array[j + 1] = temp;
+    //         }
+    //     }
+    // }
         putchar(c);
     fclose(file);
 }
 }
+ 
